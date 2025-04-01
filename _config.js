a23ffcd0d086dec2bdc8063ad23d236eed345a6c
Papsy-ops <papetua.narina@student.moringaschool.com>
2025-04-01 14:43:35 +0300

    Connection url for DB test

--- _config.js
+++ _config.js
@@ -4,7 +4,7 @@ var config = {}
 config.mongoURI = {
     production: 'mongodb+srv://<papetuanarina>:<FMySwBDqf2O93rar>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    test: 'mongodb+srv://<papetuanarina>:<FMySwBDqf2O93rar>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
 }
 module.exports = config;
 
